be828ea0664bae0770ef58ecc29ac84929102f60
JeongHyeon <0429cjhn@naver.com>
2025-05-16 22:59:49 +0900

    feat: 회원가입 폼 생성, input 컴포넌트 props 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/home/HomePage'
 import LoginPage from './pages/login/LoginPage'
+import RegisterPage from './pages/register/RegisterPage'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='' element={<HomePage />}></Route>
         <Route path='login' element={<LoginPage />}></Route>
+        <Route path='register' element={<RegisterPage />}></Route>
       </Routes>
     </>
   )
--- src/pages/login/Input.tsx
+++ src/pages/login/Input.tsx
@@ -3,22 +3,34 @@ import styles from "./Input.module.css"
 interface InputProps{
     label: string
     type: string
+    name: string
     value: string
     onChange:(e: React.ChangeEvent<HTMLInputElement>) => void; 
     placeholder: string;
+    onClick?: () => void; // ✅ 버튼 클릭 이벤트
+    buttonText?: string; // ✅ 버튼에 보여줄 텍스트
 }
-const Input = ( { label, type, value, onChange, placeholder}: InputProps)=>{
+const Input = ( { label, type, name, value, onChange, placeholder, onClick, buttonText }: InputProps)=>{
     return(
         <div>
             <div>{label}</div>
             <div className={styles.inputBox}>
                 <input 
                  type={type}
+                 name={name}
                  value={value}
                  onChange={onChange}
                  placeholder={placeholder}
-                 className={styles.input}/>
+                 className={styles.input}
+                />
+                {onClick &&(
+                <div className={styles.check} onClick={onClick}>
+                    {buttonText || "확인"} {/* ✅ 기본은 '확인', 없으면 기본값 사용 */}
+                </div>
+                )}
             </div>
          </div>
     )
 }
+
+export default Input;
