618501ac49595324d4bbe19fbd8b0a89e31d6977
JeongHyeon <0429cjhn@naver.com>
2025-05-21 18:16:25 +0900

    feat: api(axios 인스턴스) 생성 및 적용

--- src/axios/register.ts
+++ src/axios/register.ts
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
+import { api } from './axiosInstance';
+
 // 닉네임 중복 확인
 export const checkNicknameAvailable = async (nickname: string) => {
   try{
-    const response = await axios.post('http://localhost:8080/api/auth/nickname-ckeck', {
+    const response = await api.post('/auth/nickname-check', {
       nickname,
     });
     return response.data;
@@ -16,7 +18,7 @@ export const checkNicknameAvailable = async (nickname: string) => {
 // id 중복 확인
 export const checkUsernameAvailable = async (username: string) => {
   try{
-    const response = await axios.post('http://localhost:8080/api/auth/username-check', {
+    const response = await api.post('/auth/username-check', {
       username,
     });
     return response.data;
@@ -29,7 +31,7 @@ export const checkUsernameAvailable = async (username: string) => {
 // 코드 요청
 export const requestCode = async (phoneNumber: string) => {
   try {
-    const response = await axios.post('http://localhost:8080/api/auth/send-code', {
+    const response = await api.post('/auth/send-code', {
       phoneNumber,
     });
     return response.data;
@@ -42,7 +44,7 @@ export const requestCode = async (phoneNumber: string) => {
 // 코드 확인
 export const verifyCode = async (phoneNumber:string, code: string) => {
   try {
-    const response = await axios.post('http://localhost:8080/api/auth/verify-code', {
+    const response = await api.post('/auth/verify-code', {
       phoneNumber, code
     });
     return response.data;
@@ -60,7 +62,7 @@ export const responseRegister = async (
   password: string
 ) => {
   try {
-    const response = await axios.post('http://localhost:8080/api/auth/register', {
+    const response = await api.post('/auth/register', {
       nickname,
       username,
       phoneNumber,
